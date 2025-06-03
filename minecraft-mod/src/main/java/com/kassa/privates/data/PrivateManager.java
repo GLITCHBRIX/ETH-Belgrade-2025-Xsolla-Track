77531f5eec2645ca10918ec337aab244ff8c614f
Kasso4ka <serovdn22@gmail.com>
2025-06-03 23:11:50 +0300

    Added logic for set, get and delete private points in PrivateManager

--- minecraft-mod/src/main/java/com/kassa/privates/data/PrivateManager.java
+++ minecraft-mod/src/main/java/com/kassa/privates/data/PrivateManager.java
@@ -7,7 +7,8 @@ import java.util.*;
 
 public class PrivateManager {
     private static PrivateManager instance;
-    private final Map<String, BlockPos> playerSelections = new HashMap<>();
+    private final Map<String, BlockPos> playerFirstPoints = new HashMap<>();
+    private final Map<String, BlockPos> playerSecondPoints = new HashMap<>();
     
     private PrivateManager() {}
     
@@ -19,10 +20,36 @@ public class PrivateManager {
     }
     
     public void setFirstPoint(ServerPlayerEntity player, BlockPos pos) {
-        playerSelections.put(player.getUuidAsString(), pos);
+        playerFirstPoints.put(player.getUuidAsString(), pos);
     }
     
     public BlockPos getFirstPoint(ServerPlayerEntity player) {
-        return playerSelections.get(player.getUuidAsString());
+        return playerFirstPoints.get(player.getUuidAsString());
+    }
+    
+    public void setSecondPoint(ServerPlayerEntity player, BlockPos pos) {
+        playerSecondPoints.put(player.getUuidAsString(), pos);
+    }
+    
+    public BlockPos getSecondPoint(ServerPlayerEntity player) {
+        return playerSecondPoints.get(player.getUuidAsString());
+    }
+
+    public boolean hasFirstPoint(ServerPlayerEntity player) {
+        return playerFirstPoints.containsKey(player.getUuidAsString());
+    }
+    
+    public boolean hasSecondPoint(ServerPlayerEntity player) {
+        return playerSecondPoints.containsKey(player.getUuidAsString());
+    }
+    
+    public boolean hasBothPoints(ServerPlayerEntity player) {
+        return hasFirstPoint(player) && hasSecondPoint(player);
+    }
+
+    public void clearPoints(ServerPlayerEntity player) {
+        String uuid = player.getUuidAsString();
+        playerFirstPoints.remove(uuid);
+        playerSecondPoints.remove(uuid);
     }
 }
